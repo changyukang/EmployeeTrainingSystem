b796a199611aff6943f913cd7f889bd1ef24f285
a1738097 <a1738097@student.adelaide.edu.au>
2019-09-13 11:48:21 +0930

    create courses webpage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,16 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
+  def courses
+  end
+    # user = User.find_by(email: params[:session][:email].downcase)
+    #   if user && user.authenticate(params[:session][:password])
+    #     log_in user
+    #     redirect_to user
+    #   else
+    #     # Create an error message.
+    #     flash.now[:danger] = 'Invalid email/password combination'
+    #     render 'new'
+    #   end
+
 end

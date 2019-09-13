4eddec4e6802e5ecfda37ca01a0bba80fc72db7c
a1738097 <a1738097@student.adelaide.edu.au>
2019-09-13 22:30:37 +0930

    merge from courses_branch, adjust the order of head button and move the function used in static_pages into static_pages_helper

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   	get  '/help',    to: 'static_pages#help'
   	get  '/about',   to: 'static_pages#about'
   	get  '/contact', to: 'static_pages#contact'
+	get  '/courses',  to: 'static_pages#courses'
   	get  '/signup',  to: 'users#new'
   	get    '/login',   to: 'sessions#new'
   	post   '/login',   to: 'sessions#create'
-	delete '/logout',  to: 'sessions#destroyo'
-	get  '/courses',  to: 'static_pages#courses'
+	delete '/logout',  to: 'sessions#destroy'
   	resources :users
 	
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

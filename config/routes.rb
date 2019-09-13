61a99b3dc75b1e422e11681f5eb94173096954dc
a1756195 <a1756195@student.adelaide.edu.au>
2019-09-13 22:09:27 +0930

    wiki tab, wiki and quiz pages added

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   	post   '/login',   to: 'sessions#create'
 	delete '/logout',  to: 'sessions#destroyo'
 	get  '/courses',  to: 'static_pages#courses'
+	get  '/wiki',   to: 'static_pages#wiki'
+	get  '/quiz',    to: 'static_pages#quiz'
   	resources :users
 	
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

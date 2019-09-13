153007a718a5aaf33092eb3f8a063d4c537899c1
a1738097 <a1738097@student.adelaide.edu.au>
2019-09-13 17:46:50 +0930

    courses webpage merge from Course_Branch

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,24 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
+  # Returns true if the user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def courses
+    # user = User.find_by(email: params[:session][:email].downcase)
+    #   if user && user.authenticate(params[:session][:password])
+    #     log_in user
+    #     redirect_to user
+    #   else
+    #     # Create an error message.
+    #     flash.now[:danger] = 'Invalid email/password combination'
+    #     render 'new'
+    #   end
+    # @user = User.find(params[:id]
+    @users = User.all
+    # @users = current_user
+  end
+
 end

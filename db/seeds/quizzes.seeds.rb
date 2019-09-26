5672bf14b774b6067f240987e7b8cf618d2f60fd
a1756195 <a1756195@student.adelaide.edu.au>
2019-09-26 10:09:31 +0930

    seed and new quiz

--- db/seeds/quizzes.seeds.rb
+++ db/seeds/quizzes.seeds.rb
@@ -1,2 +1,4 @@
 Quiz.create!(quiz_id: 1,
             link:"localhost:3000/quiz")
+Quiz.create!(quiz_id: 2,
+            link:"localhost:3000/quiz2")
--- db/seeds/scores.seeds.rb
+++ db/seeds/scores.seeds.rb
@@ -5,12 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Score.create!(user_id: 1,
+Score.create!(user_id: 101,
              quiz_id: 2,
              score: 66)
-Score.create!(user_id: 2,
+Score.create!(user_id: 102,
              quiz_id: 2,
              score: 33)
-Score.create!(user_id: 1,
+Score.create!(user_id: 101,
              quiz_id: 1,
              score: 100)
+Score.create!(user_id: 101,
+             quiz_id: 2,
+             score: 100)
+Score.create!(user_id: 104,
+             quiz_id: 1,
+             score: 33)

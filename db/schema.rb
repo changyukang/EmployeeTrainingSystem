3b5b68e95c02d6a4e26c6f24b1f40dc5083f5de1
Daniel Baines <bainesdaniel92@gmail.com>
2019-09-29 17:27:53 +0930

    resolving schema.rb conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_09_25_091556) do
-=======
+
 ActiveRecord::Schema.define(version: 2019_09_29_013606) do
->>>>>>> group_scaffold
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"

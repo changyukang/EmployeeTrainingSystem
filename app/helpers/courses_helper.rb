5759ee97b5ef4b5a474fbd1d9f161ebb0ae2548c
Daniel Baines <bainesdaniel92@gmail.com>
2019-10-04 22:02:38 +0930

    added progress helper function

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,4 +1,9 @@
 module CoursesHelper
+
+    def get_progress(userID, courseID)
+        return CourseProgress.where(:user_id => userID, :course_id => courseID).pluck(:progress).first
+    end
+
     def get_wiki_and_quiz (courses)
             @article_wikis = Article.find_by_sql ["select * from articles where course_id = ? and category = ?", @course.course_id, "Wiki"]
             @article_quizzes = Article.find_by_sql ["select * from articles where course_id = ? and category = ?", @course.course_id, "Quiz"]

5894b510657bd22a5af751df7b8fdb8d134bd5ec
a1738097 <a1738097@student.adelaide.edu.au>
2019-10-06 12:38:24 +1030

    add multiple layer for wiki and the quiz is shown as a link in the wiki page

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,7 +1,10 @@
 module CoursesHelper
-    def get_wiki_and_quiz (courses)
+    def get_wikis (course)
             @article_wikis = Article.find_by_sql ["select * from articles where course_id = ? and category = ?", @course.course_id, "Wiki"]
-            @article_quizzes = Article.find_by_sql ["select * from articles where course_id = ? and category = ?", @course.course_id, "Quiz"]
+    end
+
+    def get_quizzes (wiki_id)
+            @article_quizzes = Quiz.find_by_sql ["select * from quizzes where article_id = ?", "#{wiki_id}"]
     end
 
     def set_wiki_global_id(wiki_id)
@@ -11,7 +14,7 @@ module CoursesHelper
 
     def get_wiki_by_id(wiki_id)
         @article_wiki_id = "#{wiki_id}"
-        @article_wiki_by_id = Article.find_by_sql ["select * from articles where id = ?", @article_wiki_id]
+        @article_wikis = Article.find_by_sql ["select * from articles where id = ?", @article_wiki_id]
     end
     
     def set_quiz_global_id(quiz_id)
@@ -20,6 +23,6 @@ module CoursesHelper
 
     def get_quiz_by_id(quiz_id)
         @article_quiz_id = "#{quiz_id}"
-        @article_quiz_by_id = Article.find_by_sql ["select * from articles where id = ?", @article_quiz_id]
+        @article_quizs = Article.find_by_sql ["select * from articles where id = ?", @article_quiz_id]
     end
 end

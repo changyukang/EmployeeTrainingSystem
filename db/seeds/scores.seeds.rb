a3d3c92062936257bd7e4ba2272d1685d58905d7
a1738097 <a1738097@student.adelaide.edu.au>
2019-10-29 20:35:08 +1030

    update seeds

--- db/seeds/scores.seeds.rb
+++ db/seeds/scores.seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Score.create!(user_id: 6, article_id: 1, score: 10)
+Score.create!(user_id: 6, article_id: 1, score: 25)
+Score.create!(user_id: 6, article_id: 1, score: 33)
+Score.create!(user_id: 6, article_id: 1, score: 40)
+Score.create!(user_id: 6, article_id: 1, score: 30)
+Score.create!(user_id: 6, article_id: 1, score: 10)
+Score.create!(user_id: 6, article_id: 1, score: 3)
+Score.create!(user_id: 6, article_id: 1, score: 40)
+Score.create!(user_id: 6, article_id: 1, score: 45)
+# Ex:- scope :active, -> {where(:active => true)}
